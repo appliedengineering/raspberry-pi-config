c33ea3d3dcd8deaabfd71e922721c5bafbb361cf
richardwei6 <richardwei6@gmail.com>
2022-04-11 01:12:21 +0100

    Improve gps script accuracy

--- Test Scripts/Working/testGPS.py
+++ Test Scripts/Working/testGPS.py
@@ -12,43 +12,18 @@ ser = serial.Serial(
 
 print("connected to: " + ser.portstr)
 
-def truncate(number, digits) -> float:
-    stepper = 10.0 ** digits
-    return math.trunc(stepper * number) / stepper
-
-def truncateWholeNumbers(f):
-   return f - int(f)
-
-def convertCoordinate(strC, strD):
-   if not len(strC):
-     print("FAILED TO CONVERT COORDINATE")
-     return 0.0
-   c = float(strC)
-   deg = int(c / 100)
-   min = int(c) % 100
-   sec = truncate(truncateWholeNumbers(c) * 100, 7)
-#   print(deg, min, sec)
-   coord = deg + min/60 + sec/3600
-
-   if(strD == 'S' or strD == 'W'):
-     coord = -coord
-   return coord
-
 try:
    while True:
       raw = ser.readline()
 #      if len(raw) > 0:
 #          print(raw)
       if (raw.startswith(b"$GPGGA")):
-         msg = pynmea2.parse(raw.decode("utf-8"))
-         print(msg.lat, msg.lon)
+         try:
+             msg = pynmea2.parse(raw.decode("utf-8"))
+         except:
+             print("Failed to parse")
 
-         if (len(msg.lat) < 0 or len(msg.lon) < 0): # invalid data parsed
-            continue
-
-         lat = convertCoordinate(msg.lat, msg.lat_dir)
-         lon = convertCoordinate(msg.lon, msg.lon_dir)
-         print(lat, lon)
+         print(msg.latitude, msg.longitude)
 
       time.sleep(0.01)
 except KeyboardInterrupt:
